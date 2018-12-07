3d61ca5bafccd414b3a059275400fec9e6e9d4c6
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-07 21:00:26 +0100

    start python from 0

---
+++ src/1-print/print.py
@@ -0,0 +1,11 @@
+print(1, 2, 3) #1 2 3
+
+print('Belhassen Gharsallah', 'bel7aG', sep = '\n', end = '\n\n')
+#output:
+# Belhassen Gharsallah
+# bel7aG
+
+
+#To use formatted string literals, begin a string with f or F before the opening quotation mark or triple quotation mark.
+print(f'okokokokok  {1 + 1}')
+# ==> f
