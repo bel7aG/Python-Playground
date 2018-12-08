290311524a2525ecde90598c1a8d911d32956e57
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-08 21:44:05 +0100

    wrapup 1 %

--- src/Wrapup.py
+++ src/Wrapup.py
@@ -9,12 +9,25 @@ name = 'Belhassen'
 surname = 'Gharsallah'
 print("\n\nName: ", name, "\nSurname: ", surname, '\n')
 
-#                               Operators
+#                               Operators for Numbers
 
                            #   + - * / % // **   #
+
+isThisMyFirstNumber = 1996
+
 print('8 + 4 = ', 8 + 4)
 print('8 - 4 = ', 8 - 4)
 print('8 / 4 = ', 8 / 4)
 print('8 % 4 = ', 8 % 4)
 print('8 // 4 = ', 8 // 4)
 print('8 ** 4 = ', 8 ** 4)
+
+
+thinkLikeItIsYouFirstString = "\"is python better than Javascript dont think so"
+
+print(thinkLikeItIsYouFirstString)
+
+# Multi line String
+bel7aG = '''\nMy Name is Belhssen Gharsallah
+            you can call me bel7aG'''
+print('\n', bel7aG, end = '\n\n')
